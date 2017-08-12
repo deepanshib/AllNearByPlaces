ae1c56d40a1bdec8f065ebed6e7994f19359f60f
deepanshib <deepanshibansal@ymail.com>
2017-08-12 11:37:42 +0530

    nearby places showing

--- app/src/main/java/com/deepanshi/allnearbyplaces/MainActivity.java
+++ app/src/main/java/com/deepanshi/allnearbyplaces/MainActivity.java
@@ -1,20 +1,141 @@
 package com.deepanshi.allnearbyplaces;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+    protected GoogleApiClient mGoogleApiClient;
+    private ArrayList<Place> place = new ArrayList<>();
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    double clong, clat;
+    LocationManager loc;
+    LocationListener loclis;
+    list adapter = new list(place, clong, clat);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.rv);
+
+        loc = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        loclis = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                clong = location.getLongitude();
+                clat = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        loc.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 500, loclis);
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv);
         recyclerView.setHasFixedSize(false);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new list());
+        recyclerView.setAdapter(adapter);
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+        mGoogleApiClient.connect();
+        retrievePlaces();
+    }
+    private void retrievePlaces() throws SecurityException {
+
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null);
+
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(@NonNull PlaceLikelihoodBuffer likelyPlaces) {
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    place.add(placeLikelihood.getPlace().freeze());
+                    Log.i("Place = ",  placeLikelihood.getPlace().getName().toString());
+                }
+                adapter.notifyDataSetChanged();
+//                likelyPlaces.release();
+            }
+        });
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            // permission has been granted, continue as usual
+////            Log.i("Client Connection", "Connected to GoogleClient");
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Google Places API connection failed with error code:" + connectionResult.getErrorCode(), Toast.LENGTH_LONG).show();
+
+    }
+    // Handle the result from requested permissions.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],@NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.i("Permision", "permission granted");
+                }
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/com/deepanshi/allnearbyplaces/list.java
+++ app/src/main/java/com/deepanshi/allnearbyplaces/list.java
@@ -1,54 +1,231 @@
 package com.deepanshi.allnearbyplaces;
-
+import android.location.Location;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.maps.model.LatLng;
+import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * Created by Deepanshi Bansal on 06-08-2017.
  */
+public class list extends RecyclerView.Adapter<list.CardViewHolder>  {
+    ArrayList<Place> places = new ArrayList<>();
+    double clong,clat;
 
-public class list extends RecyclerView.Adapter<list.CardViewHolder> {
-int number;
-//    public list(int num){
-//        number=num;
-//    }
+    public list(ArrayList<Place> places,double clong,double clat) {
+        this.places = places;
+        this.clat=clat;
+        this.clong=clong;
+    }
+
     @Override
     public list.CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LinearLayout linearLayout=(LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview,parent,false);
-        CardViewHolder cv= new CardViewHolder(linearLayout);
+        View linearLayout = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview, parent, false);
+        CardViewHolder cv = new CardViewHolder(linearLayout);
         return cv;
     }
+
     @Override
     public void onBindViewHolder(list.CardViewHolder holder, int position) {
-    holder.setText(position);
+        holder.setText(position);
+//      holder.telephone.setText(places.get(position).getPhoneNumber().toString());
+//        holder.website.setText((CharSequence) places.get(position).getWebsiteUri().toString());
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return places.size();
     }
 
-    public static class CardViewHolder extends RecyclerView.ViewHolder {
-        TextView name,address,distance,type;
-        public CardViewHolder(LinearLayout itemView) {
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+        TextView name, address, distance, type;
+
+        public CardViewHolder(View itemView) {
             super(itemView);
-            name=(TextView)itemView.findViewById(R.id.name);
-            address=(TextView)itemView.findViewById(R.id.address);
-            distance=(TextView)itemView.findViewById(R.id.distance);
-            type=(TextView)itemView.findViewById(R.id.type);
+            name = (TextView) itemView.findViewById(R.id.name);
+            address = (TextView) itemView.findViewById(R.id.address);
+            distance = (TextView) itemView.findViewById(R.id.distance);
+            type = (TextView) itemView.findViewById(R.id.type);
 
         }
-        public void setText(int pos){
 
-            name.setText("deepanshi"+pos);
-            address.setText("deepali"+pos);
-            type.setText("public");
-            distance.setText(2+pos+"km");
+        public void setText(int position) {
+            name.setText(places.get(position).getName().toString());
+            address.setText(places.get(position).getAddress().toString());
+            type.setText(type(places.get(position).getPlaceTypes().get(0)));
+            if (places.get(position).getPlaceTypes().size() == 2) {
+                type.append(",\n" + type(places.get(position).getPlaceTypes().get(1)));
+            }
+            if (places.get(position).getPlaceTypes().size() == 3) {
+                type.append(",\n" + type(places.get(position).getPlaceTypes().get(1)));
+                type.append(",\n" + type(places.get(position).getPlaceTypes().get(2)));
+            }
+            LatLng latlng = places.get(position).getLatLng();
+            float[] results = new float[1];
+            Location.distanceBetween(clat, clong,
+                    latlng.latitude,latlng.longitude, results);
+            float dist = results[0];
+            distance.setText(dist/1000000+" km");
+        }
+        public String type(int list){
+            if(list<100) {
+                HashMap<Integer, String> hashmap = new HashMap<>();
+                String[] str = {"accounting",
+                        "Airport",
+                        "Amusement_park",
+                        "Aquarium",
+                        "Art_gallery",
+                        "Atm",
+                        "Bakery",
+                        "Bank",
+                        "Bar",
+                        "Beauty_salon",
+                        "Bicycle_store",
+                        "book_store",
+                        "bowling_alley",
+                        "bus_station",
+                        "cafe",
+                        "campground",
+                        "car_dealer",
+                        "car_rental",
+                        "car_repair",
+                        "car_wash",
+                        "casino",
+                        "cemetery",
+                        "church",
+                        "city_hall",
+                        "clothing_store",
+                        "convenience_store",
+                        "courthouse",
+                        "dentist",
+                        "department_store",
+                        "doctor",
+                        "electrician",
+                        "electronics_store",
+                        "embassy",
+                        "establishment",
+                        "finance",
+                        "fire_station",
+                        "florist",
+                        "food/restaurant",
+                        "funeral_home",
+                        "furniture_store",
+                        "gas_station",
+                        "general_contractor",
+                        "grocery_or_supermarket",
+                        "gym",
+                        "hair_care",
+                        "hardware_store",
+                        "health",
+                        "hindu_temple",
+                        "home_goods_store",
+                        "hospital",
+                        "insurance_agency",
+                        "jewelry_store",
+                        "laundry",
+                        "lawyer",
+                        "library",
+                        "liquor_store",
+                        "local_government_office",
+                        "locksmith",
+                        "lodging",
+                        "meal_delivery",
+                        "meal_takeaway",
+                        "mosque",
+                        "movie_rental",
+                        "movie_theater",
+                        "moving_company",
+                        "museum",
+                        "night_club",
+                        "painter",
+                        "Park",
+                        "Parking",
+                        "Pet_store",
+                        "Pharmacy",
+                        "physiotherapist",
+                        "place_of_worship",
+                        "plumber",
+                        "Police",
+                        "Post_office",
+                        "real_estate_agency",
+                        "Restaurant",
+                        "Roofing_contractor",
+                        "rv_park",
+                        "School",
+                        "Shoe_store",
+                        "Shopping_mall",
+                        "spa",
+                        "stadium",
+                        "storage",
+                        "Store",
+                        "subway_station",
+                        "synagogue",
+                        "taxi_stand",
+                        "Train_station",
+                        "Travel_agency",
+                        "University",
+                        "veterinary_care",
+                        "zoo"};
+                for (int i = 1; i <= 96; i++) {
+                    hashmap.put(i, str[i - 1]);
+                }
+                return hashmap.get(list);
+            }
+            else {
+                HashMap<Integer, String> map = new HashMap<>();
+                String[] str2 = {"administrative_area1",
+                        "administrative_area2",
+                        "administrative_area3",
+                        "administrative_area4",
+                        "administrative_area5",
+                        "colloquial_area",
+                        "Country",
+                        "establishment",
+                        "finance",
+                        "Floor",
+                        "Food",
+                        "general_contractor",
+                        "Society",
+                        "Health",
+                        "intersection",
+                        "Locality",
+                        "Natural_feature",
+                        "Neighborhood",
+                        "Place_of_worship",
+                        "Political",
+                        "Point_of_interest",
+                        "Post_box",
+                        "Postal_code",
+                        "postal_code_prefix",
+                        "postal_code_suffix",
+                        "postal_town",
+                        "premise",
+                        "Room",
+                        "Route",
+                        "Street_address",
+                        "Street_number",
+                        "sublocality",
+                        "sublocality 4",
+                        "sublocality_5",
+                        "sublocality_3",
+                        "sublocality_2",
+                        "sublocality_1",
+                        "subpremise"};
+                for (int i = 1001; i <= 1030; i++) {
+                    map.put(i, str2[i - 1001]);
+
+                }
+                return map.get(list);
+            }
         }
     }
 }
